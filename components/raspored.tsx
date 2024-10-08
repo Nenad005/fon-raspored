1561d7580f2c39b8bcbc080386e5b9b4e4f50a2c
Nenad005 <markonenadovic2005@gmail.com>
2024-10-08 02:13:43 +0200

    prod push

--- components/raspored.tsx
+++ components/raspored.tsx
@@ -50,7 +50,7 @@ function yearName(year) {
 export default function Raspored({raspored, group}) {
   const [day, setDay] = useState(nazivDana())
   const [error, setError] = useAtom(errorAtom)
-  console.log(error ? '' : 'test')
+  console.log(error ? '' : '')
   setError(false)
 
   return <>
