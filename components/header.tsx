0779a2942b814d2a3346d98481ea18f577d20678
Nenad005 <markonenadovic2005@gmail.com>
2024-11-01 15:36:10 +0100

    logo href

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,10 @@ export default function Header() {
   return <>
     <header className="w-full flex justify-center border-b">
       <div className="flex justify-start w-full md:w-[760px] items-center px-5 py-2 gap-3">
-        <img  src="https://oas.fon.bg.ac.rs/wp-content/uploads/2023/04/FON-Logo-Tamni.png.webp"
-              className="h-16 aspect-auto dark:brightness-0 dark:invert mr-auto"></img>
+        <a className="h-16 aspect-auto mr-auto" href="https://oas.fon.bg.ac.rs/raspored-nastave/" target="_blank">
+          <img  src="https://oas.fon.bg.ac.rs/wp-content/uploads/2023/04/FON-Logo-Tamni.png.webp"
+                className="w-full h-full dark:brightness-0 dark:invert"></img>
+        </a>
         <ModeToggle></ModeToggle>
         <SettingButton></SettingButton>
       </div>
